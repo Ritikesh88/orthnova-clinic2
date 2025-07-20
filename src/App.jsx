7672d532be0033df4d6e53f63ae9ab10cd90a030
Ritikesh88 <ritikesh.mahapatra@gmail.com>
2025-07-20 17:53:02 +0530

    2nd login Removed

--- src/App.jsx
+++ src/App.jsx
@@ -1395,8 +1395,7 @@ export default function App() {
   ) : (
     <div className="col-span-3 text-center py-16 bg-white rounded-lg shadow">
       <p className="text-lg text-gray-700 mb-4">Please log in to access the clinic system</p>
-      <Login onLogin={() => setSession(true)} />
-    </div>
+         </div>
   )}
 </main>
 
