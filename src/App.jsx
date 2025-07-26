bd3cb71d406b2e840f7c1c2cca856961e8dcc0d4
Ritikesh88 <ritikesh.mahapatra@gmail.com>
2025-07-26 14:47:02 +0530

    doctor

--- src/App.jsx
+++ src/App.jsx
@@ -1497,8 +1497,7 @@ export default function App() {
             {hasRole('doctor') && (
               <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <PrescriptionForm />
-                <BillHistory />
-              </div>
+                </div>
             )}
           </div>
         ) : (
